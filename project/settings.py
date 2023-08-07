38c908bba8430409df0e65fb031121ae87d93af0
Arwinder Singh <76734033+Arwinder-Singh@users.noreply.github.com>
2023-08-08 02:07:41 +0530

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-_xj#ui!flk34k-#vi!dm#3&7+(k8yd*gl0l@^k0_@6&vmkmb@&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["OrangeShop.pythonanywhere.com"]
 
 
 # Application definition
@@ -117,13 +117,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT= os.path.join(BASE_DIR,'static/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS=[BASE_DIR/'static']
+# STATIC_URL = 'static/'
+# STATICFILES_DIRS=[BASE_DIR/'static']
 
 AUTH_USER_MODEL='home.User'
