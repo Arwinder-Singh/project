ce76c01c24a51725b880ec1f7a015463510f7a10
tashu014 <ramlal7788456@gmail.com>
2023-08-15 16:59:09 +0530

    Initial commit

--- home/forms.py
+++ home/forms.py
@@ -3,18 +3,28 @@ from .models import User
 from django.contrib.auth.forms import UserCreationForm 
 
 class loginForm(forms.Form):
-    username=forms.CharField(widget=forms.TextInput(attrs={"class":"form-control"}))
-    password=forms.CharField(widget=forms.PasswordInput(attrs={"class":"form-control"}))
+    username=forms.CharField(widget=forms.TextInput(attrs={"class":"form-control", 'placeholder': 'xyz'}))
+    password=forms.CharField(widget=forms.PasswordInput(attrs={"class":"form-control", 'placeholder': '********'}))
     
       
       
 class signupForm(UserCreationForm):
-    username=forms.CharField(widget=forms.TextInput(attrs={"class":"form-control"}))
-    password1=forms.CharField(widget=forms.PasswordInput(attrs={"class":"form-control"}))
-    password2=forms.CharField(widget=forms.PasswordInput(attrs={"class":"form-control"}))
-    email=forms.CharField(widget=forms.TextInput(attrs={"class":"form-control"}))
-    
+    username = forms.CharField(widget=forms.TextInput(attrs={"class": "form-control", 'placeholder': 'xyz'}))
+    password1 = forms.CharField(label="Password",widget=forms.PasswordInput(attrs={"class": "form-control", 'placeholder': '********'}))
+    password2 = forms.CharField(label="Retype password",widget=forms.PasswordInput(attrs={"class": "form-control", 'placeholder': '********'}))
+    email = forms.EmailField(widget=forms.EmailInput(attrs={"class": "form-control", 'placeholder': 'xyz@email.com'}))
+    IS_OWNER_CHOICES = (
+        (True, "Owner"),
+        (False, "Customer"),
+    )
+    is_owner = forms.ChoiceField(
+        label="Account Type",
+        choices=IS_OWNER_CHOICES,
+        widget=forms.RadioSelect(attrs={"class": "form-check-input"})
+    )
+
     class Meta:
-        model=User
-        fields=('username','email','password1','password2','is_owner','is_customer')
+        model = User
+        fields = ("username", "email", "password1", "password2", "is_owner")
+
           
